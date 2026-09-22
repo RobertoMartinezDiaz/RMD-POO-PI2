32a9ed2cc920f147081ef792dc9408510a903788
RobertoMartinezDiaz <rmartinezd@unicartagena.edu.co>
2026-09-22 13:43:09 -0500

    feat(ejercicio1): crear clase Producto con visibilidad de paquete y verificar acceso interno

---
+++ Taller 5/ejercicio1/Producto.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+
+public class Producto {
+
+    String nombre;
+    double precio;
+    int stock;
+
+    Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+
+    }
+
+    void mostrarInfo(){
+        System.out.println("Nombre: " +  nombre + ", Precio: " + precio + ", Stock: " + stock);
+    }
+}
---
+++ Taller 5/ejercicio1/Prueba.java
@@ -0,0 +1,9 @@
+package ejercicio1;
+
+public class Prueba {
+    public static void main(String[] args){
+        Producto producto = new Producto("Medias", 25.000, 12);
+
+        producto.mostrarInfo();
+    }
+}
